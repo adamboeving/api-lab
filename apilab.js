8c7da7779a1a1fd48a9545e7c882571537816b73
adamboeving <adamboeving@yahoo.com>
2022-03-09 20:49:55 -0500

    formatted document

--- apilab.js
+++ apilab.js
@@ -1,58 +1,33 @@
-(function(){
-    const textInput = document.querySelector('#textInput');
-    const pushButton = document.querySelector('#pushButton');
-    const ul = document.querySelector('#redditList');
-    let test = 'aww';
+(function () {
+  const textInput = document.querySelector("#textInput");
+  const pushButton = document.querySelector("#pushButton");
+  const ul = document.querySelector("#redditList");
+  let test = "aww";
 
+  function pageLoad() {
+    fetch(`https://www.reddit.com/r/${test}/.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const list = document.querySelector("#redditList");
+        for (let i = 0; i < data.data.children.length; i++) {
+          let redditInfo = document.createElement("li");
+          redditInfo.innerHTML = `<img src=${data.data.children[i].data.thumbnail}>${data.data.children[i].data.title}: <a href=${data.data.children[i].data.url}>  ${data.data.children[i].data.url}</a>`;
 
-    function pageLoad(){
-
-        fetch(`https://www.reddit.com/r/${test}/.json`)
-        .then(response => response.json())
-        .then(data => {
-
-        console.log(data)
-        const list = document.querySelector('#redditList');
-        for (let i = 0; i < data.data.children.length; i++){
-                let redditInfo = document.createElement('li')
-                redditInfo.innerHTML = `<img src=${data.data.children[i].data.thumbnail}>${data.data.children[i].data.title}: <a href=${data.data.children[i].data.url}>  ${data.data.children[i].data.url}</a>`
-            // redditInfo.innerText = `${data.data.children[i].data.title}: ${data.data.children[i].data.url}`
-                
-                list.append(redditInfo)
-                console.log(data.data.children[i].data)
-
+          list.append(redditInfo);
+          console.log(data.data.children[i].data);
         }
-    })
+      });
+  }
 
-    }
+  pageLoad();
+
+  pushButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    test = textInput.value;
+    ul.replaceChildren();
 
     pageLoad();
-
-    pushButton.addEventListener('click', event => {
-        event.preventDefault();
-
-        test = textInput.value;
-        ul.replaceChildren();
-      
-        pageLoad();
-    
-    })
-
-// fetch(`https://www.reddit.com/r/${test}/.json`)
-// .then(response => response.json())
-// .then(data => {
-
-//     console.log(data)
-//     const list = document.querySelector('#redditList');
-//     for (let i = 0; i < data.data.children.length; i++){
-//         let redditInfo = document.createElement('li')
-//         redditInfo.innerHTML = `<img src=${data.data.children[i].data.thumbnail}>${data.data.children[i].data.title}: <a href=${data.data.children[i].data.url}>  ${data.data.children[i].data.url}</a>`
-//        // redditInfo.innerText = `${data.data.children[i].data.title}: ${data.data.children[i].data.url}`
-        
-//         list.append(redditInfo)
-//         console.log(data.data.children[i].data)
-
-//     }
-// })
-
+  });
 })();
